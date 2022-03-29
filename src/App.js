4c5cebf52ce6ef8d368710dfca9eeeda07f92cc1
CarlTaoLiu <carl.liu@silksoftware.com>
2022-03-29 11:18:26 +0800

    second commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { useCallback, useEffect, useState } from 'react'
+
+
+function App() {
+  const [counter,setCounter] = useState(0)
+  const [name,setName] = useState('carl')
+  const handlePlus = useCallback(()=>{
+      console.log(counter)
+      setCounter(counter+1)
+    },[counter]
+    
+  )
+  const changeName = useCallback(()=>{
+    setName(Math.random().toString())
+    
+  },[name])
+  useEffect(()=>{
+    document.title = `你已经点击${counter}次了`
+  },[counter]
+  )
+  useEffect(()=>{
+    if(counter>0){setName(Math.random().toString())}
+    
+  },[counter])
+  return(
+    <div>
+      {counter}
+      <button onClick={handlePlus}>
+        下一页
+      </button>
+      {name}
+      <button onClick={changeName}>
+        页面{counter}
+      </button>
+    </div>
+  )
+
+}
+
+export default App;
